31c703c60c9126922d45e7d3ee78568bf77a7c80
bhavyank89 <bhavyanksarolia@gmail.com>
2024-12-28 19:34:49 +0530

    easy_0118_pascalTringle

---
+++ Cpp_leetCode_problems/easy_0118_pascalTringle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> generateRow(int row){
+        vector<int> ans;
+        int temp = 1;
+        ans.push_back(temp);
+        for(int i=1;i<row;i++){
+            temp = temp*(row-i);
+            temp = temp / i;
+            ans.push_back(temp);
+        }
+        return ans;
+    };
+    vector<vector<int>> generate(int numRows) {
+      vector<vector<int>> ans;
+      for(int i=1;i<=numRows;i++){
+        ans.push_back(generateRow(i));
+      }  
+      return ans;
+    }
+};
