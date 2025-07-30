c3e6032775a4139f2cc71b2dfcc3ec7075178ce5
bhavyank89 <bhavyanksarolia@gmail.com>
2025-07-30 15:06:09 +0530

    easy_1796_secondLargestDigitInString

---
+++ Cpp_leetCode_problems/easy_1796_secondLargestDigitInString.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        set<int> digits;
+        for (char c : s) {
+            if (isdigit(c)) {
+                digits.insert(c - '0');
+            }
+        }
+
+        if (digits.size() < 2) return -1;
+
+        auto it = digits.rbegin(); // reverse iterator, largest element
+        ++it; // move to second largest
+        return *it;
+    }
+};
